ebb04297fb8383aade5ad945ebcd8552cc0b0337
Alejo Martina <alejomartina477@gmail.com>
2023-09-27 19:22:00 -0300

    Created PHP practice

---
+++ PHP/Part 1/ex5/visitas.php
@@ -0,0 +1,12 @@
+<!-- Página que va a contener al contador de visitas -->
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Contador de visitas</title>
+</head>
+<body>
+    <? include("contador.php")?> 
+</body>
+</html>
